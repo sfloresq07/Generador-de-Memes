b79430c78463b13103addb25554bc5831dc8c077
Sandra Flores <sfloresq07@gmail.com>
2023-10-24 15:33:36 -0300

    cambio de estrucutura y funcionabilidad de panel de html de panel de aside imagen

--- js/index.js
+++ js/index.js
@@ -10,4 +10,35 @@ $(".change-theme").addEventListener("click", () => {
   }
 });
 
+//Aside imagen
 
+$(".bottom--imagen").addEventListener("click", () => {
+  $("#asideImage").style.width = "100%";
+  $("#memeGeneratorContainer").style.display = "none";
+  $("#buttonClose").style.width = "20px";
+  $("#asideText").style.width = "0";
+});
+
+//Aside texto
+$(".bottom--text").addEventListener("click", () => {
+  $("#asideText").style.width = "100%";
+  $("#memeGeneratorContainer").style.display = "none";
+  $("#buttonClose").style.display = "flex";
+  $("#asideImage").style.width = "0";
+  console.log("aside text");
+});
+
+$("#buttonClose").addEventListener("click", () => {
+  $(".buttonClose").style.display = "none";
+  $("#asideText").style.width = "0";
+  $("#memeGeneratorContainer").style.display = "flex";
+
+  console.log("aside text quitar");
+});
+
+$("#btnClose").addEventListener("click", () => {
+    $("#btnClose").style.display = "none";
+    $("#asideImage").style.width = "0";
+    $("#memeGeneratorContainer").style.display = "flex";
+    console.log("aside text quitar");
+  });
