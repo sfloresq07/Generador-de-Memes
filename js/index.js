d5c06ec91027171878ac1183e8f26add9e74ab99
Sandra Flores <sfloresq07@gmail.com>
2023-10-24 23:03:58 -0300

    funcionabilidad a la color de fuente y contenedor

--- js/index.js
+++ js/index.js
@@ -10,6 +10,11 @@ $(".change-theme").addEventListener("click", () => {
   }
 });
 
+$(".change-theme").addEventListener("click", () => {
+  $(".theme-dark").classList.toggle("hidden");
+  $(".theme-light").classList.toggle("hidden");
+});
+
 //Aside imagen
 
 $(".bottom--imagen").addEventListener("click", () => {
@@ -21,78 +26,110 @@ $(".bottom--imagen").addEventListener("click", () => {
 
 //Aside texto
 $(".bottom--text").addEventListener("click", () => {
-    $("#asideText").style.width = "100%";
-    $("#memeGeneratorContainer").style.display = "none";
-    $("#buttonClose").style.display = "flex";
-    $("#asideImage").style.width = "0";
-    console.log("aside text");
-  });
+  $("#asideText").style.width = "100%";
+  $("#memeGeneratorContainer").style.display = "none";
+  $("#buttonClose").style.display = "flex";
+  $("#asideImage").style.width = "0";
+  console.log("aside text");
+});
 
 //boton de cerrar panel de texto
 
 $("#buttonClose").addEventListener("click", () => {
-    $(".buttonClose").style.display = "none";
-    $("#asideText").style.width = "0";
-    $("#memeGeneratorContainer").style.display = "flex";
-  
-    console.log("aside text quitar");
-  });
+  $(".buttonClose").style.display = "none";
+  $("#asideText").style.width = "0";
+  $("#memeGeneratorContainer").style.display = "flex";
+
+  console.log("aside text quitar");
+});
 
 // boton de cerrar panel de imagen
-  $("#btnClose").addEventListener("click", () => {
-      $("#btnClose").style.display = "none";
-      $("#asideImage").style.width = "0";
-      $("#memeGeneratorContainer").style.display = "flex";
-      console.log("aside text quitar");
-    });
+$("#btnClose").addEventListener("click", () => {
+  $("#btnClose").style.display = "none";
+  $("#asideImage").style.width = "0";
+  $("#memeGeneratorContainer").style.display = "flex";
+  console.log("aside text quitar");
+});
 
 // Modificacion de top text
 $("#topText").addEventListener("input", (e) => {
-    $(".paragraphtopText").innerText = e.target.value
-  })
+  $(".paragraphtopText").innerText = e.target.value;
+});
 
-  // Modificacion de top text
+// Modificacion de top text
 $("#bottomText").addEventListener("input", (e) => {
-    $(".paragraphBottomText").innerText = e.target.value
-  })
+  $(".paragraphBottomText").innerText = e.target.value;
+});
 
 //sin texto superior
 
-$("#noTopText").addEventListener("input", (e) =>{
-    if(e.target.checked) {
-        $(".memeGeneratorContainer__topText").style.display ="none"
-    } else{
-        $(".memeGeneratorContainer__topText").style.display ="flex" 
-    }
-})
+$("#noTopText").addEventListener("input", (e) => {
+  if (e.target.checked) {
+    $(".memeGeneratorContainer__topText").style.display = "none";
+  } else {
+    $(".memeGeneratorContainer__topText").style.display = "flex";
+  }
+});
 
 //sin texto inferior
-$("#noBottomText").addEventListener("input", (e) =>{
-    if(e.target.checked) {
-        $(".memeGeneratorContainer__bottomText").style.display ="none"
-    } else{
-        $(".memeGeneratorContainer__bottomText").style.display ="flex" 
-    }
-})
+$("#noBottomText").addEventListener("input", (e) => {
+  if (e.target.checked) {
+    $(".memeGeneratorContainer__bottomText").style.display = "none";
+  } else {
+    $(".memeGeneratorContainer__bottomText").style.display = "flex";
+  }
+});
 
 // Tipo de fuente
 
 $("#fontType").addEventListener("input", (e) => {
-    $("#paragraphtopText").style.fontFamily = e.target.value
-  })
+  $("#paragraphtopText").style.fontFamily = e.target.value;
+});
 
-  $("#fontType").addEventListener("input", (e) => {
-    $("#paragraphBottomText").style.fontFamily = e.target.value
-  })
+$("#fontType").addEventListener("input", (e) => {
+  $("#paragraphBottomText").style.fontFamily = e.target.value;
+});
 
-  // Tamaño de fuente
-  $("#fontSize").addEventListener("input", (e) => {
-    $("#paragraphtopText").style.fontSize = `${e.target.value}px`
-})
+// Tamaño de fuente
+$("#fontSize").addEventListener("input", (e) => {
+  $("#paragraphtopText").style.fontSize = `${e.target.value}px`;
+});
 
 $("#fontSize").addEventListener("input", (e) => {
-  $("#paragraphBottomText").style.fontSize = `${e.target.value}px`
+  $("#paragraphBottomText").style.fontSize = `${e.target.value}px`;
+});
+
+//Alineación de texto
+
+$("#alignLeft").addEventListener("click", () => {
+  $("#containerTopText").style.textAlign = "left";
+  $("#containerBottomText").style.textAlign = "left";
+});
+
+$("#alignCenter").addEventListener("click", () => {
+  $("#containerTopText").style.textAlign = "center";
+  $("#containerBottomText").style.textAlign = "center";
+});
+
+$("#alignRight").addEventListener("click", () => {
+  $("#containerTopText").style.textAlign = "right";
+  $("#containerBottomText").style.textAlign = "right";
+});
+
+// Color de fuente
+
+$("#textColor").addEventListener("input", (e) => {
+   $("#paragraphtopText").style.color = e.target.value 
 })
 
-  
+$("#textColor").addEventListener("input", (e) => {
+    $("#paragraphBottomText").style.color = e.target.value 
+ })
 
+ $("#backgroungColor").addEventListener("input", (e) =>{
+    $("#containerTopText").style.backgroundColor = e.target.value  
+ })
+
+ $("#backgroungColor").addEventListener("input", (e) =>{
+    $("#containerBottomText").style.backgroundColor = e.target.value  
+ })
